c6dc6296ac581bdfa22c01673218e2e02e9842ac
lijialong <lijial@flatincbr.com>
2023-08-29 09:57:47 +0800

    feat: feat

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import ImageCompressor from "./modules/image-compressor";
 import logger from "./modules/image-compressor/utils/logger";
+import * as sharp from "sharp";
+
+logger.info("sharp", sharp);
 
 export function activate(context: vscode.ExtensionContext) {
   const imageEffecter = new ImageCompressor(context);
